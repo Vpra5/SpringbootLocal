39005b1a03fa6d29bcd40ace085c607ecf2abd2d
Vpra5 <89732811+Vpra5@users.noreply.github.com>
2022-12-13 14:51:12 +0530

    Add files via upload

---
+++ springwebdemo/src/main/java/com/axis/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.axis.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TestController {
+	
+	ArrayList<String> fruits = new ArrayList<String>();
+	
+	public TestController() {
+		
+		fruits.add("apple");
+		fruits.add("mango");
+		fruits.add("banana");
+	}
+		
+		@GetMapping("/fruits")
+		public List<String> getAllFruits()
+		{
+			return fruits;
+		
+		}
+		
+
+}
